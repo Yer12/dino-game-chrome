f4f6b449740e94ceb35aa47785b58886436b1b26
Erlan <erlan.u@1fit.app>
2022-05-06 12:32:33 +0600

    asz

--- dino.js
+++ dino.js
@@ -7,8 +7,6 @@ import {
 const dinoElem = document.querySelector("[data-dino]")
 const JUMP_SPEED = 0.45
 const GRAVITY = 0.0015
-const DINO_FRAME_COUNT = 2
-const FRAME_TIME = 100
 
 let isJumping
 let dinoFrame

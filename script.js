016088090c3e6e4fedbd8c44a406586bc1d4fe50
Erlan <erlan.u@1fit.app>
2022-05-13 11:28:29 +0600

    ssaax

--- script.js
+++ script.js
@@ -1,95 +1,45 @@
-import { updateGround, setupGround } from "./ground.js"
-import { updateDino, setupDino, getDinoRect, setDinoLose } from "./dino.js"
-import { updateCactus, setupCactus, getCactusRects } from "./cactus.js"
+const phone = document.getElementById("phone")
+const name = document.getElementById("name")
 
-const WORLD_WIDTH = 100
-const WORLD_HEIGHT = 30
-const SPEED_SCALE_INCREASE = 0.00001
+IMask(
+    document.getElementById('phone'), {
+        mask: '+{7}(000)000-00-00'
+    });
 
-const worldElem = document.querySelector("[data-world]")
-const scoreElem = document.querySelector("[data-score]")
-const startScreenElem = document.querySelector("[data-start-screen]")
 
-setPixelToWorldScale()
-window.addEventListener("resize", setPixelToWorldScale)
-if(checkLose) {
-  document.addEventListener("keydown", handleStart, { once: true })
-  document.addEventListener("click", handleStart, { once: true })
+
+function handlePaste(e) {
+    let pastedData = e.clipboardData.getData('text');
+    console.log((pastedData).slice(-10))
+    return pastedData.slice(-10);
 }
 
-let lastTime
-let speedScale
-let score
-function update(time) {
-  if (lastTime == null) {
-    lastTime = time
-    window.requestAnimationFrame(update)
-    return
-  }
-  const delta = time - lastTime
+phone.addEventListener('paste', handlePaste)
 
-  updateGround(delta, speedScale)
-  updateDino(delta, speedScale)
-  updateCactus(delta, speedScale)
-  updateSpeedScale(delta)
-  updateScore(delta)
-  if (checkLose()) return handleLose()
+const formatPhone = (phone) => {
+    const newPhone = phone.replace(/\D/g, '').slice(-10);
 
-  lastTime = time
-  window.requestAnimationFrame(update)
-}
+    // +77774442233
+    return `+7${newPhone}`;
+};
 
-function checkLose() {
-  const dinoRect = getDinoRect()
-  return getCactusRects().some(rect => isCollision(rect, dinoRect))
-}
 
-function isCollision(rect1, rect2) {
-  return (
-    rect1.left < rect2.right &&
-    rect1.top < rect2.bottom &&
-    rect1.right > rect2.left &&
-    rect1.bottom > rect2.top
-  )
-}
 
-function updateSpeedScale(delta) {
-  speedScale += delta * SPEED_SCALE_INCREASE
-}
-
-function updateScore(delta) {
-  score += delta * 0.01
-  scoreElem.textContent = Math.floor(score)
-}
-
-function handleStart() {
-  lastTime = null
-  speedScale = 1
-  score = 0
-  setupGround()
-  setupDino()
-  setupCactus()
-  startScreenElem.classList.add("hide")
-  window.requestAnimationFrame(update)
-}
-
-function handleLose() {
-  setDinoLose()
-  setTimeout(() => {
-    document.addEventListener("keydown", handleStart, { once: true })
-    document.addEventListener("click", handleStart, { once: true })
-    startScreenElem.classList.remove("hide")
-  }, 100)
-}
-
-function setPixelToWorldScale() {
-  let worldToPixelScale
-  if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
-    worldToPixelScale = window.innerWidth / WORLD_WIDTH
-  } else {
-    worldToPixelScale = window.innerHeight / WORLD_HEIGHT
-  }
-
-  worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`
-  worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`
+function checkUser() {
+    if(name.value.length === 0 || phone.value.length === 0) {
+        alert("Fill form correctly !")
+    }
+    else {
+        if(phone.value.length === 16) {
+            localStorage.setItem('user', JSON.stringify({
+                phone: formatPhone(phone.value),
+                name: name.value,
+                score: 0
+            }))
+            window.location.href="game.html";
+        }
+        else {
+            alert("phone is not correct", phone.value.length)
+        }
+    }
 }

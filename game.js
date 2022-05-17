22ce2d5ad6bbecf8841318a4aa5beb22103e9138
Erlan <erlan.u@1fit.app>
2022-05-17 15:45:27 +0600

    feat(main page): layout

--- game.js
+++ game.js
@@ -107,14 +107,14 @@ function handleLose() {
 
 }
 
-function setPixelToWorldScale() {
-  let worldToPixelScale
-  if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
-    worldToPixelScale = window.innerWidth / WORLD_WIDTH
-  } else {
-    worldToPixelScale = window.innerHeight / WORLD_HEIGHT
-  }
-
-  worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`
-  worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`
-}
+// function setPixelToWorldScale() {
+//   let worldToPixelScale
+//   if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
+//     worldToPixelScale = window.innerWidth / WORLD_WIDTH
+//   } else {
+//     worldToPixelScale = window.innerHeight / WORLD_HEIGHT
+//   }
+//
+//   worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`
+//   worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`
+// }

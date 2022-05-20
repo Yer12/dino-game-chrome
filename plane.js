0b7c5261bc8ab2ef2310e44304457544648ece4d
Erlan <erlan.u@1fit.app>
2022-05-20 11:48:38 +0600

    feat(main page): layout

--- plane.js
+++ plane.js
@@ -9,7 +9,7 @@ const planeElems = document.querySelectorAll("[data-plane]")
 const planeTexts = [
     'Just jump!',
     'уан фииит',
-    'Беги, Лола, беги!',
+    'Беги,Лола,беги!',
     'Накачай пальцы ',
     'Работай руками! ',
     'Ты сможешь!',
@@ -19,8 +19,12 @@ const planeTexts = [
 
 
 export function setupPlane() {
-    setCustomProperty(planeElems[0], "--left", -700)
-    setCustomProperty(planeElems[1], "--left", -1400)
+    setCustomProperty(planeElems[0], "--left", -200)
+    setCustomProperty(planeElems[1], "--left", -400)
+    setCustomProperty(planeElems[2], "--left", -600)
+    setCustomProperty(planeElems[3], "--left", -800)
+    setCustomProperty(planeElems[4], "--left", -1000)
+    setCustomProperty(planeElems[5], "--left", -1200)
 }
 
 export function updatePlane(delta, speedScale) {

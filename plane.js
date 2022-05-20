34a71c11f02cb221e3df77e7510182d85f8e1e9a
Erlan <erlan.u@1fit.app>
2022-05-20 09:51:49 +0600

    feat(main page): layout

--- plane.js
+++ plane.js
@@ -4,7 +4,7 @@ import {
     setCustomProperty,
 } from "./updateCustomProperty.js"
 
-const SPEED = 0.009
+const SPEED = 0.09999
 const planeElems = document.querySelectorAll("[data-plane]")
 const planeTexts = [
     'Just jump!',
